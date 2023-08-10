524a33e172cc5a4d50f9cfe9ac04bb3ac8d7761f
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-08-10 01:26:19 -0300

    Arquivos iniciais e infra (tabela dynamo)

---
+++ infra/dynamo.tf
@@ -0,0 +1,32 @@
+resource "aws_dynamodb_table" "music_table" {
+  name         = "tb_music"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "Artist"
+  range_key    = "SongTitle"
+
+  attribute {
+    name = "Artist"
+    type = "S"
+  }
+
+  attribute {
+    name = "SongTitle"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table_item" "music_item" {
+  table_name = aws_dynamodb_table.music_table.name
+  hash_key   = aws_dynamodb_table.music_table.hash_key
+  range_key  = aws_dynamodb_table.music_table.range_key
+  item       = <<ITEM
+{
+  "Artist": {"S": "The Guess Who"},
+  "SongTitle": {"S": "These Eyes"},
+  "WrittenBy": {"L": [{"S": "Burton Cummings"}, {"S": "Randy Bachman"}]},
+  "ProducedBy": {"L": [{"S": "Nimbus 9"}, {"S": "Jack Richardson"}]},
+  "Album": {"S": "Wheatfield Soul"},
+  "ReleasedIn": {"N": "1969"}
+}
+ITEM
+}
---
+++ src/test/java/com/github/gasfgrv/dynamodbtest/DynamodbTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gasfgrv.dynamodbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamodbTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
