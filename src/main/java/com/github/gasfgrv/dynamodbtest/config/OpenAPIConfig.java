4c300130654b3bdfd36f71c0c1f8bd0c0b4b53d0
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-08-15 22:38:57 -0300

    Configurações de OpenAPI

--- src/main/java/com/github/gasfgrv/dynamodbtest/config/OpenAPIConfig.java
+++ src/main/java/com/github/gasfgrv/dynamodbtest/config/OpenAPIConfig.java
@@ -1,4 +1,36 @@
 package com.github.gasfgrv.dynamodbtest.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        var server = new Server();
+        server.setUrl("http://localhost:8080");
+        server.setDescription("Server URL in Development environment");
+
+        var contact = new Contact();
+        contact.setEmail("gustavo_almeida11@hotmail.com");
+        contact.setName("Gustavo Silva");
+        contact.setUrl("https://github.com/gasfgrv");
+
+        var info = new Info()
+                .title("Music API")
+                .version("1.0")
+                .contact(contact)
+                .description("This api serves as a test for the aws sdk for dynamodb functions (query, load and save).");
+
+        return new OpenAPI()
+                .info(info)
+                .servers(List.of(server));
+    }
+
 }
